1684c645aa4c8909c8cb1ea43bacb2ad2da54ae8
Sadeq Moballeqoleslam <sadeq.moballeqoleslam@gmail.com>
2023-10-10 01:35:25 +0330

    init project

---
+++ app/frame/[address]/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function FramePage() {
+  return <div>FramePage</div>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <button className="btn btn-lg btn-primary">Connect Wallet</button>
+    </div>
+  );
+}
