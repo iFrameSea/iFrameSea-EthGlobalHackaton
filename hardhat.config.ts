91ca7b270bd9e97ebf2f32cdea40ad35912d9e95
Sadeq Moballeqoleslam <sadeq.moballeqoleslam@gmail.com>
2023-10-22 02:52:03 +0330

    change solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.18',
+  solidity: '0.8.20',
   networks: {
     local: {
       url: 'http://127.0.0.1:7545',
